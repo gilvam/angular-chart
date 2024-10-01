0778fc0fe42dabc32564c346f7d3d5338861ed6c
Gilvam Mourao <gilvamm@gmail.com>
2024-10-01 18:41:37 -0300

    chore: prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,27 @@ import { ArrayUtil } from '@util/array.util';
 	standalone: true,
 	imports: [RouterOutlet, ChartLineComponent, ChartDonutHalfComponent],
 	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 	chartLineTextList: number[] = [];
+
 	chartLineData = [
 		[0, 170.2, 180.1, 170.7, 200, 500, 600, 600],
 		[50.01, 100.2, 280.1, 370.7, 360.8, 500.7, 0],
-		[50, 100, 250, 300, 390, 300, 20, '10'],
+		[50, 100, 250, 300, 390, 300, 20, '10']
 	];
+
+	flags = {
+		dynamic: true,
+		tree: false
+	};
 
 	ngOnInit() {
 		this.chartLineTextList = ArrayUtil.scaled(this.chartLineData, 6);
+
+		(Object.keys(this.flags) as Array<keyof typeof this.flags>).forEach((key) => {
+			this.flags[key] = true;
+		});
 	}
 }
